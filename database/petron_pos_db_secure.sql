46ad677237ba8f3fba51e255c68b13e77d0ac9c8
SandaraPagaling <amda.cabahug.coc@phinmaed.com>
2026-09-17 21:43:50 +0800

    ywamongtanann

--- database/petron_pos_db_secure.sql
+++ database/petron_pos_db_secure.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 16, 2026 at 05:32 PM
+-- Generation Time: Sep 16, 2026 at 05:43 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
